3c818fb00a89e99ea30fc97223098525c625f633
Rodrigo <rodrigorael53@gmail.com>
2021-02-23 20:57:37 -0300

    add the css modules

--- front-end/src/components/ExperienceBar.tsx
+++ front-end/src/components/ExperienceBar.tsx
@@ -1,11 +1,16 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
+// vai criar no className caracteres unicos para que o css nunca interfira em
+// outros componentes da aplicação, o css vai ficar exclusivo a esse excopo
+
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
         <div style={{ width: '50%' }} />
 
-        <span className="current-experience" style={{ left: '50%' }}>
+        <span className={styles.currentExperience} style={{ left: '50%' }}>
           300 xp
         </span>
       </div>

10feff627bcce6801f9ae47a787b500994a982c8
Rodrigo <rodrigorael53@gmail.com>
2021-02-23 23:40:02 -0300

    add page name and favicon.png

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,18 +1,26 @@
 import { CompletedChanllenges } from '../components/CompletedChanllenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Move.It</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChanllenges />
+          <Countdown />
         </div>
 
         <div></div>
